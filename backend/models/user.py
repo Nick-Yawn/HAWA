795b8a910ddd9c31d91e22aef0d47e6e7cc158a9
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-11 13:50:02 -0500

    update user model to reflect projects, role, username > name

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,15 +1,20 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from enum import Enum
 
+class RoleEnum(Enum):
+    user = 1
+    admin = 2
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
+    id              = db.Column(db.Integer, primary_key=True)
+    name            = db.Column(db.String(40), nullable=False)
+    email           = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    role            = db.Column(db.Enum(RoleEnum), nullable=False, default="user")
 
     projects = db.relationship('Project', back_populates='user')
 
@@ -27,11 +32,11 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
+            'name': self.name,
             'email': self.email
         }
 
     def projects_to_dict(self):
         return {
-            'projects': [p.to_dict for p in self.projects]
+            'projects': [p.to_dict() for p in self.projects]
                 }
