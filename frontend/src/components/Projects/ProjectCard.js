03b8820844e1101b7cf5d6351cd7d8231ca22a46
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-11 15:09:30 -0500

    add Projects and ProjectCard components

---
+++ frontend/src/components/Projects/ProjectCard.js
@@ -0,0 +1,10 @@
+
+export default function ProjectCard({ project }) {
+  
+
+  return (
+    <div className="project-card">
+      {project.title} 
+    </div>
+  )
+}
---
+++ frontend/src/components/Projects/index.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import ProjectCard from './ProjectCard';
+import { readProjects } from '../../store/projects';
+
+import './Projects.css'
+
+export default function Projects() {
+  const dispatch = useDispatch();
+  const projects = useSelector(state => state.projects)
+  const projectsExist = projects?.length > 0; 
+
+  useEffect(()=> {
+    dispatch(readProjects())
+  }, [dispatch])
+
+  return (
+    <div className="project-card-container">
+      {projectsExist && (
+        projects.map(project => <ProjectCard project={project} /> )
+      )}
+    </div>
+  )
+}
