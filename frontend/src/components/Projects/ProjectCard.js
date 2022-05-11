282c4120209e5085f638c7c770ce209595698094
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-11 15:35:01 -0500

    style projects view, basic

--- frontend/src/components/Projects/ProjectCard.js
+++ frontend/src/components/Projects/ProjectCard.js
@@ -1,9 +1,13 @@
+import './ProjectCard.css'
+import { useHistory } from 'react-router-dom';
 
 export default function ProjectCard({ project }) {
-  
+  const history = useHistory();
+
+  const handleClick = e => history.push(`/projects/${project.id}`) 
 
   return (
-    <div className="project-card">
+    <div className="project-card" onClick={handleClick}>
       {project.title} 
     </div>
   )
