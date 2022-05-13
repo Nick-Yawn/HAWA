3fc6d76e92cf9472a74e3eba9775ee3bc94b9ce9
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-13 17:14:44 -0500

    add various ProjectCard animations

--- frontend/src/components/Projects/ProjectCard.js
+++ frontend/src/components/Projects/ProjectCard.js
@@ -3,24 +3,31 @@ import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 
-export default function ProjectCard({ project }) {
+// this component is either simply a link to the single project page, or a form to create a new project
+
+export default function ProjectCard(props) {
+  const { project, projectSelected, setProjectSelected, redirectToProject, formActive, setFormActive } = props;
   const history = useHistory();
-  const [ formActive, setFormActive ] = useState(false);
-  const [ showCard, setShowCard ] = useState(true);
+  const [ selectedCardIsThisCard, setSelectedCardIsThisCard ] = useState(false);
+  const isCreateCard = project === undefined;
 
-  const handleClick = e => {
-    if( project !== undefined ){ 
-      history.push(`/projects/${project.id}`)
-    } else {
+  const action = e => {
+    if( isCreateCard ){
       setFormActive(!formActive);
+    } else {
+      setSelectedCardIsThisCard(true);
+      setProjectSelected(project.id);
     }
-  } 
+  }
 
-  const className = formActive ? "project-card project-card-active" : "project-card"
+  const className = formActive && isCreateCard ? "project-card project-card-active" : "project-card";
 
   return (
-    <CSSTransition in={showCard} timeout={500} onExited={handleClick} classNames="project-card">
-      <div className={className} onClick={()=>setShowCard(false)}>
+    <CSSTransition  in={(!projectSelected && !formActive) || (isCreateCard && formActive)} 
+                    timeout={500} 
+                    onExited={redirectToProject} 
+                    classNames={selectedCardIsThisCard ? "selected-project" : "project-card"}>
+      <div className={className} onClick={action}>
         {project?.title || "Start a Project"}
       </div>
     </CSSTransition>
--- frontend/src/components/Projects/index.js
+++ frontend/src/components/Projects/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import ProjectCard from './ProjectCard';
 import { readProjects } from '../../store/projects';
 
@@ -7,6 +8,10 @@ import './Projects.css'
 
 export default function Projects() {
   const dispatch = useDispatch();
+  const history  = useHistory();
+  const [ projectSelected, setProjectSelected ] = useState(null);
+  const [ formActive, setFormActive ] = useState(false);
+  const [ cardsHidden, setCardsHidden ] = useState(false);
   const projects = useSelector(state => state.projects)
   const projectsExist = projects?.length > 0; 
 
@@ -14,12 +19,35 @@ export default function Projects() {
     dispatch(readProjects())
   }, [dispatch])
 
+  useEffect(()=>{
+    if(formActive)
+      setTimeout( () => setCardsHidden(true), 500 )
+  },[formActive])
+
+  const redirectToProject = () => {
+    if( !formActive )
+      history.push(`/projects/${projectSelected}`)
+  };
+
+  // the first card is not provided a project prop and therefore becomes the new project form
   return (
     <div className="project-card-container">
-      <ProjectCard />
-      {projectsExist && (
-        projects.map(project => <ProjectCard project={project} /> )
-      )}
+      <ProjectCard  projectSelected={projectSelected} 
+                    setProjectSelected={setProjectSelected}
+                    redirectToProject={redirectToProject}
+                    formActive={formActive}
+                    setFormActive={setFormActive}
+                    />
+      {projectsExist && !cardsHidden && (
+        projects.map(project => 
+          <ProjectCard  project={project} 
+                        projectSelected={projectSelected} 
+                        setProjectSelected={setProjectSelected}
+                        redirectToProject={redirectToProject}
+                        formActive={formActive}
+                        setFormActive={setFormActive}
+                        /> 
+      ))}
     </div>
   )
 }
