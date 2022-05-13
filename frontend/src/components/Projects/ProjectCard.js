22b9fdfcc1d8933ba4576267ad78aadd3038640e
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-13 13:35:33 -0500

    add single project component and basic ProjectCard unmount animations

--- frontend/src/components/Projects/ProjectCard.js
+++ frontend/src/components/Projects/ProjectCard.js
@@ -1,10 +1,12 @@
 import './ProjectCard.css'
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 
 export default function ProjectCard({ project }) {
   const history = useHistory();
   const [ formActive, setFormActive ] = useState(false);
+  const [ showCard, setShowCard ] = useState(true);
 
   const handleClick = e => {
     if( project !== undefined ){ 
@@ -17,8 +19,10 @@ export default function ProjectCard({ project }) {
   const className = formActive ? "project-card project-card-active" : "project-card"
 
   return (
-    <div className={className} onClick={handleClick}>
-      {project?.title || "Start a Project"} 
-    </div>
+    <CSSTransition in={showCard} timeout={500} onExited={handleClick} classNames="project-card">
+      <div className={className} onClick={()=>setShowCard(false)}>
+        {project?.title || "Start a Project"}
+      </div>
+    </CSSTransition>
   )
 }
