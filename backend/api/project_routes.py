1cced92dc831b2149c95349c38200e961058ceb3
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-16 10:44:59 -0500

    add delete project route

--- backend/api/project_routes.py
+++ backend/api/project_routes.py
@@ -6,12 +6,13 @@ from backend.forms.project_form import ProjectForm
 project_routes = Blueprint('projects', __name__)
 
 
-
+# get all projects
 @project_routes.route('/', methods = ['GET'])
 @login_required
 def get_user_projects():
     return current_user.projects_to_dict();
 
+# create project
 @project_routes.route('/', methods = ['POST'])
 @login_required
 def post_project():
@@ -24,6 +25,20 @@ def post_project():
         db.session.commit()
         return project.to_dict();
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+# delete
+@project_routes.route('/<int:id>', methods = ['DELETE'])
+@login_required
+def delete_project(id):
+    project = Project.query.get(id)
+
+    if project.user_id != current_user.id:
+        return {'errors': ["Access Denied"]}, 403
+
+    db.session.delete(project)
+    db.session.commit()
+
+    return {'id':id, 'message':'delete successful'}
 
 @project_routes.errorhandler(500)
 def internal_server_error(e):
