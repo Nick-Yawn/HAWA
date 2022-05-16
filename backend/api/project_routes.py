dc61899391c0445d6b85f393c15cb8dba0331afd
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-15 23:21:56 -0500

    add post project route and 500 error handler

--- backend/api/project_routes.py
+++ backend/api/project_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_login import current_user, login_required
 from backend.models import User, Project, db
+from backend.forms.project_form import ProjectForm
 
 project_routes = Blueprint('projects', __name__)
 
@@ -11,4 +12,19 @@ project_routes = Blueprint('projects', __name__)
 def get_user_projects():
     return current_user.projects_to_dict();
 
+@project_routes.route('/', methods = ['POST'])
+@login_required
+def post_project():
+    form = ProjectForm()
 
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        project = Project( title=form.data['title'], user_id=current_user.id)
+        db.session.add(project)
+        db.session.commit()
+        return project.to_dict();
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+@project_routes.errorhandler(500)
+def internal_server_error(e):
+    return {'errors': ["Internal Server Error"]}, 500
