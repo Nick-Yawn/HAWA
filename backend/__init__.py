e5bd10fae241a0eeefda1f0b4b29370ac091428b
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-15 23:19:44 -0500

    add app-wide 405 handler

--- backend/__init__.py
+++ backend/__init__.py
@@ -70,3 +70,7 @@ def react_root(path):
     if path == 'favicon.ico':
         return app.send_static_file('favicon.ico')
     return app.send_static_file('index.html')
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return {"errors": ["Method Not Allowed"]}, 405
