353688de59829f28aae0b49d09648f40383a5520
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-15 23:24:50 -0500

    add post project behavior to project slice file

--- frontend/src/store/projects.js
+++ frontend/src/store/projects.js
@@ -1,9 +1,15 @@
 
-const READ_PROJECTS = 'projects/GET_PROJECTS'
+const GET_PROJECTS = 'projects/GET_PROJECTS'
+const POST_PROJECT = 'projects/POST_PROJECT'
 
-const readProjectsAction = projects => ({
-  type: READ_PROJECTS,
+const getProjectsAction = projects => ({
+  type: GET_PROJECTS,
   projects
+})
+
+const postProjectAction = project => ({
+  type: POST_PROJECT,
+  project
 })
 
 export const readProjects = () => async dispatch => {
@@ -12,18 +18,40 @@ export const readProjects = () => async dispatch => {
   const data = await response.json();
 
   if( response.ok ){
-    await dispatch(readProjectsAction(data.projects));
+    await dispatch(getProjectsAction(data.projects));
     return data.projects;
   } else {
     console.log(data.errors);
   }
 } 
 
+export const postProject = project => async dispatch => {
+  const response = await fetch('/api/projects/', {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(project)
+  })
+
+  const data = await response.json();
+
+  if( response.ok ){
+    await dispatch(postProjectAction(data))
+    return data;
+  } else {
+    console.log(data.errors);
+    return {id: null}
+  }
+
+}
+
 export default function projects(state = [], action) {
   const newState = [...state];
   switch( action.type ){
-    case READ_PROJECTS:
+    case GET_PROJECTS:
       return [ ...action.projects ]
+    case POST_PROJECT:
+      return [...newState, action.project]
+      return newState;
     default:
       return newState;
   }
