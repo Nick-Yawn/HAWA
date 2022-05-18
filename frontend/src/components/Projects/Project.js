fca649593bc3074cd4c02ead2a4021fe75aabfc8
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-18 17:19:20 -0500

    add rough project page structure

--- frontend/src/components/Projects/Project.js
+++ frontend/src/components/Projects/Project.js
@@ -1,9 +1,18 @@
 //import { useEffect, useState } from 'react';
 //import { useParams } from 'react-router-dom';
 
+import './Project.css';
+
 export default function Project() {
 //  const {project_id} = useParams();
   return (
-    <></>
+    <div className="sidebar-and-main-container">
+      <div className="project-sidebar">
+        
+      </div>
+      <div className="project-main">
+        
+      </div>
+    </div>
   );
 }
