772b66ef2292154a7fe361d32b7c93b34df9c7d5
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-20 08:56:43 -0500

    fix splash button

--- frontend/src/components/Splash.js
+++ frontend/src/components/Splash.js
@@ -1,8 +1,9 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import './Splash.css';
 
 export default function Splash() {
+  const history = useHistory();
 
   const copyString = "HAWA makes CRUD-app planning easy. Create Projects, then add Features, User Stories, Routes, and Acceptance Criteria. Finally, export them to wiki friendly GitHub Flavored Markdown, coming soon!";
 
@@ -18,7 +19,9 @@ export default function Splash() {
         <span className="splash-text splash-copy">Acceptance Criteria*</span>
         <span className="splash-text splash-copy">with painless conversion to pre-formatted wiki docs.*</span>
         <span className="splash-text splash-copy coming-soon">*Coming soon!</span>
-        <Link to='/sign-up' className="splash-signup-link">— Get Started —</Link>
+        <button onClick={()=>history.push('/sign-up')} className="get-started-button">
+         Get Started
+        </button>
         <div className="splash-footer-container">
           <div className="splash-text splash-credits">
             <span className="splash-credit-text">An original work by Nick Yuan</span>
