9333d7b6b91ca7a0621b515ee1e9fb93ae41aaa6
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-21 13:10:13 -0500

    add new facet form

--- frontend/src/components/Projects/Facet.js
+++ frontend/src/components/Projects/Facet.js
@@ -1,21 +1,75 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 import './Facet.css';
 
-export default function Facet({ facet, type, links, setLinks }) {
-
-  // separate facet-form component, later  
+export default function Facet(props) {
+  const { facet,
+          type,
+          links,
+          setLinks,
+          aFormActive,
+          setAFormActive} = props;
+  const [ formActive, setFormActive ] = useState(false);
+  const [ name, setName ] = useState('');
+  const formRef = useRef(null);
  
-  // add this facet to sidebar 
+  // add this facet, if it exists to sidebar 
   useEffect(()=>{
-    setLinks( prevLinks => [...prevLinks, facet.name] )
+    if( facet ) setLinks( prevLinks => [...prevLinks, facet.name] )
   },[])
 
-  return (
+
+  useEffect(()=>{
+    if( !aFormActive ){
+      setFormActive(false);
+    }
+  },[aFormActive])
+
+  useEffect(()=>{
+    if( formActive )
+      formRef.current.focus();
+  },[formActive])
+
+  const showForm = e => {
+    e.stopPropagation();
+    setFormActive(true);
+    setAFormActive(true);
+  }
+
+  const updateName = e => setName(e.target.value);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+
+  if( facet ) return (
     <div className="facet-container">
       <div className="facet-header">
+        <div className="facet-name">
         { facet.name }
+        </div>
       </div>
+
+
+    </div>
+  ); else return (
+    <div className="facet-container">
+      <div className="facet-header facet-form" onClick={showForm}>
+        {formActive || <div className="facet-name add-facet-label">+ Add a Feature</div> }
+        {formActive && (
+          <form onSubmit={handleSubmit}>
+            <input  type="text"
+                    ref={formRef}
+                    value={name}
+                    className="facet-input"
+                    onChange={updateName}
+            />
+          </form>
+        )}
+      </div>
+
+      
     </div>
   );
 }
