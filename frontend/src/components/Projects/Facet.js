72150ac32f52d0772196dd4bc67ac6692037c8f1
Nicholas Yuan <NicholasNYuan@gmail.com>
2022-05-21 10:44:49 -0500

    init Facet component

---
+++ frontend/src/components/Projects/Facet.js
@@ -0,0 +1,5 @@
+
+
+export default function Facet({ ,links, setLinks }) {
+
+}
--- frontend/src/components/Projects/Project.js
+++ frontend/src/components/Projects/Project.js
@@ -1,18 +1,31 @@
-//import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 //import { useParams } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 
 import './Project.css';
 
 export default function Project() {
 //  const {project_id} = useParams();
+  
+  // state slice holds sidebar links, each first render of facet adds to links
+  const [ links, setLinks ] = useState([]);
+  
+
   return (
-    <div className="sidebar-and-main-container">
-      <div className="project-sidebar">
-        
+    <CSSTransition  in={true}
+                    appear={true}
+                    timeout={1000}
+                    classNames={"project-page"}>
+      <div className="sidebar-and-main-container">
+        <div className="project-sidebar">
+          {links.map(link => 
+            <div className={`sidebar-link ${link.className}`}>{link}</div>
+          )}
+        </div>
+        <div className="project-main">
+          
+        </div>
       </div>
-      <div className="project-main">
-        
-      </div>
-    </div>
+    </CSSTransition>
   );
 }
